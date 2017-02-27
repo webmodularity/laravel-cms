c490d4ffc1708677c598acbd6c8ee64176713927
Rory Hellyer <rory@webmodularity.com>
2017-02-27 14:32:01 -0800

    dev

--- resources/views/navbar/user-menu.blade.php
+++ resources/views/navbar/user-menu.blade.php
@@ -24,7 +24,7 @@
                 </tr>
                 @foreach($activeUserRecentLogins as $userLogin)
                     <tr>
-                        <td><small>{{ $userLogin->logRequest->created_at->format('m/d/y h:ia') }}</small></td>
+                        <td><small>{{ $userLogin->logRequest->created_at }}</small></td>
                         <td><small>{{ $userLogin->logRequest->ipAddress->ip_address }}</small></td>
                         <td><small>{{ $userLogin->getVia() }}</small></td>
                     </tr>
