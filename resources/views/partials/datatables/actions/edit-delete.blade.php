a85b40c9a1ce08bf0d76cb37fb9116f7f90cb5e1
Rory Hellyer <rory@webmodularity.com>
2017-03-10 04:10:15 -0800

    dev

--- resources/views/partials/datatables/actions/edit-delete.blade.php
+++ resources/views/partials/datatables/actions/edit-delete.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('regions.destroy', ['id' => $id]) }}" method="post">
 {!! csrf_field() !!}
 {{ method_field('DELETE') }}
-    <a href="{{ route('regions.edit', ['id' => $id])  }}" class="btn btn-sm btn-primary">Edit</a>
-    <button type="submit" class="btn btn-sm btn-danger"><i class="fa fa-trash-o"></i>&nbsp;Delete</button>
+    <a href="{{ route('regions.edit', ['id' => $id])  }}" class="btn btn-sm btn-primary pull-left"><i class="fa fa-edit"></i>&nbsp;Edit</a>
+    <button type="submit" class="btn btn-sm btn-danger pull-right"><i class="fa fa-trash-o"></i>&nbsp;Delete</button>
 </form>
