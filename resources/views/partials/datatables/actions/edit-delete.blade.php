91b1cbdc2565a5a8728606ad993821da7d21ad5f
Rory Hellyer <rory@webmodularity.com>
2017-03-11 02:04:29 -0800

    dev

--- resources/views/partials/datatables/actions/edit-delete.blade.php
+++
@@ -1,4 +0,0 @@
-<div style="white-space: nowrap">
-    <a href="{{ route('regions.edit', ['id' => $id])  }}" class="btn btn-xs btn-primary"><i class="fa fa-edit"></i>&nbsp;Edit</a>
-    <button type="button" class="btn btn-xs btn-danger delete-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $name }}"><i class="fa fa-trash-o"></i>&nbsp;Delete</button>
-</div>
