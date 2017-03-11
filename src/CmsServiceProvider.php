66d01c8defbeea60e12855c16025ec89d0ed41a9
Rory Hellyer <rory@webmodularity.com>
2017-03-11 15:45:26 -0800

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -91,7 +91,7 @@ class CmsServiceProvider extends ServiceProvider
         dom: "<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>f>>" +
         "<'row'<'col-sm-12'tr>>" +
         "<'row'<'col-sm-5'i><'col-sm-7'p>>",
-        initComplete => function() {
+        initComplete: function() {
             $('#dataTableBuilder_filter').appendTo( $('.box-tools:eq(0)') );
         }
     });
