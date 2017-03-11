71750595473e58c7eb73fc0b18a75db22ef118c3
Rory Hellyer <rory@webmodularity.com>
2017-03-11 15:44:53 -0800

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -90,7 +90,10 @@ class CmsServiceProvider extends ServiceProvider
         },
         dom: "<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>f>>" +
         "<'row'<'col-sm-12'tr>>" +
-        "<'row'<'col-sm-5'i><'col-sm-7'p>>"
+        "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+        initComplete => function() {
+            $('#dataTableBuilder_filter').appendTo( $('.box-tools:eq(0)') );
+        }
     });
 </script>
 EOT;
