e42e2f782c16b8ce65f88182d8077c3de40c21a1
Rory Hellyer <rory@webmodularity.com>
2017-03-11 01:58:51 -0800

    dev

--- src/DataTables/CmsDataTable.php
+++ src/DataTables/CmsDataTable.php
@@ -18,4 +18,52 @@ class CmsDataTable extends DataTable
                 $('#dataTableBuilder').DataTable().buttons().container()
                     .appendTo( $('#dataTableBuilder_wrapper div:eq(1)') );\n";
     }
+
+    protected function getDeleteConfirmAlert()
+    {
+        return "$('.delete-confirm-button').click(function(){
+    var id = $(this).data(\"id\");
+    var token = $(this).data(\"token\");
+    var recordIdent = $(this).data('record-ident');
+    var dtApi = new $.fn.dataTable.Api( settings );
+    swal({
+        title: 'Delete This Record?',
+        text: recordIdent,
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonClass: 'btn-danger',
+        confirmButtonText: 'Yes, delete it!',
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true
+    },
+    function() {
+        $.ajax({
+            url: location.pathname.replace(/\/+$/, '') + '/' +id,
+            method: 'POST',
+            data: {
+                '_method': 'DELETE',
+                '_token': token,
+            },
+            success: function () {
+                swal({
+                    title: 'Successfully Deleted Record',
+                    text: recordIdent,
+                    type: 'success',
+                    timer: 3000,
+                    confirmButtonClass: 'btn-primary',
+                });
+                dtApi.ajax.reload(null, false);
+            },
+            error: function () {
+                swal({
+                    title: 'Delete Failed!',
+                    text: 'An unknown server error was encountered when attempting to delete this record. ',
+                    type: 'error',
+                    confirmButtonClass: 'btn-primary',
+                });
+            }
+        });
+    });
+});";
+    }
 }
