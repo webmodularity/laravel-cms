dbdcf5ef4b2ea843a96e414f8092dc93cbfebba9
Rory Hellyer <rory@webmodularity.com>
2017-03-11 15:15:25 -0800

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -79,22 +79,21 @@ class CmsServiceProvider extends ServiceProvider
 
     private function loadBlade()
     {
-        Blade::directive('dt-defaults', function () {
-            return "<?php <<< EOT
+        Blade::directive('dtdefaults', function () {
+            return <<< EOT
 <script>
     $.extend(true, $.fn.dataTable.defaults, {
         language: {
-            search: \"<div class='has-feedback'>_INPUT_<span class='glyphicon glyphicon-search form-control-feedback'></span></div>\",
-            searchPlaceholder: \"Search...\",
-            lengthMenu: \"Results per page: _MENU_\"
+            search: "<div class="has-feedback">_INPUT_<span class="glyphicon glyphicon-search form-control-feedback"></span></div>\",
+            searchPlaceholder: "Search...",
+            lengthMenu: "Results per page: _MENU_"
         },
-        dom: \"<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>f>>\" +
-        \"<'row'<'col-sm-12'tr>>\" +
-        \"<'row'<'col-sm-5'i><'col-sm-7'p>>\"
+        dom: "<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>f>>" +
+        "<'row'<'col-sm-12'tr>>" +
+        "<'row'<'col-sm-5'i><'col-sm-7'p>>"
         });
 </script>
 EOT;
-?>";
         });
     }
 }
