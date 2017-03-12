4446ace59a03ba3ec12f0af0470e886a19914c51
Rory Hellyer <rory@webmodularity.com>
2017-03-12 01:41:33 -0800

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -97,9 +97,7 @@ class CmsServiceProvider extends ServiceProvider
         dom: \"<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>f>>\" +
         \"<'row'<'col-sm-12'tr>>\" +
         \"<'row'<'col-sm-5'i><'col-sm-7'p>>\",
-        initComplete: function() {
-        }";
-
+        initComplete: function() {";
             foreach ($inits as $init) {
                 $script .= $init . "\n";
             }
