188a51ebc0bcedc79e600caef3fe0da59bd41bd2
Rory Hellyer <rory@webmodularity.com>
2017-03-11 19:52:24 -0800

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -85,11 +85,11 @@ class CmsServiceProvider extends ServiceProvider
 <script>
     $.extend(true, $.fn.dataTable.defaults, {
         language: {
-            search: "<div class='has-feedback'>_INPUT_<span class='glyphicon glyphicon-search form-control-feedback'></span></div>",
+            search: "",
             searchPlaceholder: "Search...",
             lengthMenu: "Results per page: _MENU_"
         },
-        dom: "f<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>>>" +
+        dom: "<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>f>>" +
         "<'row'<'col-sm-12'tr>>" +
         "<'row'<'col-sm-5'i><'col-sm-7'p>>",
         initComplete: function() {
