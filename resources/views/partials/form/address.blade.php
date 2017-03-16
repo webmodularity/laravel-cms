e7ab0829d816663a46c2ca74986c57f7f89eafb6
Rory Hellyer <rory@webmodularity.com>
2017-03-16 00:36:28 -0700

    dev

--- resources/views/partials/form/address.blade.php
+++ resources/views/partials/form/address.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group {{ $errors->has('address.street') ? 'has-error' : '' }}">
-            <label class="control-label" for="address.street">Address</label>
-            <input type="text" name="address.street" class="form-control" value="{{ $street }}" placeholder="Street Address" />
+            <label class="control-label" for="address[street]">Address</label>
+            <input type="text" name="address[street]" class="form-control" value="{{ $street }}" placeholder="Street Address" />
             @if ($errors->has('address.street'))
                 <span class="help-block">
                             <strong>{{ $errors->first('address.street') }}</strong>
@@ -14,7 +14,7 @@
 <div class="row">
     <div class="col-sm-5">
         <div class="form-group {{ $errors->has('address.city') ? 'has-error' : '' }}">
-            <input type="text" name="address.city" class="form-control" value="{{ $city }}" placeholder="City" />
+            <input type="text" name="address[city]" class="form-control" value="{{ $city }}" placeholder="City" />
             @if ($errors->has('address.city'))
                 <span class="help-block">
                     <strong>{{ $errors->first('address.city') }}</strong>
@@ -24,7 +24,7 @@
     </div>
     <div class="col-sm-3">
         <div class="form-group {{ $errors->has('address.state_id') ? 'has-error' : '' }}">
-            <select class="form-control" style="width: 100%;" name="address.state_id" id="address-state_id" data-placeholder="State">
+            <select class="form-control" style="width: 100%;" name="address[state_id]" id="address-state_id" data-placeholder="State">
                 <option></option>
                 @foreach($stateList as $state)
                     <option value="{{ $state['id'] }}"{{ $state_id == $state['id'] ? ' selected' : '' }}>{{ $state['iso'] }}</option>
@@ -39,7 +39,7 @@
     </div>
     <div class="col-sm-4">
         <div class="form-group {{ $errors->has('address.zip') ? 'has-error' : '' }}">
-            <input type="text" name="address.zip" class="form-control" value="{{ $zip }}"
+            <input type="text" name="address[zip]" class="form-control" value="{{ $zip }}"
                    placeholder="Zip" />
             @if ($errors->has('address.zip'))
                 <span class="help-block">
