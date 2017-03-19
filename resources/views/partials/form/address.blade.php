4e9d8f7102c00c2f06e962c4130bbcd570898e1b
Rory Hellyer <rory@webmodularity.com>
2017-03-19 15:24:30 -0700

    dev

--- resources/views/partials/form/address.blade.php
+++ resources/views/partials/form/address.blade.php
@@ -10,7 +10,7 @@ $addressSettings[$addressField] = [
         <div class="form-group {{ $errors->has($addressField . '.street') ? 'has-error' : '' }}">
             <label class="control-label" for="{{ $addressField }}[street]">{{ $addressSettings[$addressField]['label'] }}</label>
             <div class="input-group">
-                <input type="text" name="{{ $addressField }}[street]" class="form-control" value="{{ $street }}" placeholder="Street Address"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }} />
+                <input type="text" name="{{ $addressField }}[street]" id="{{ $addressField }}-street" class="form-control" value="{{ $street }}" placeholder="Street Address"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }} />
                 <span class="input-group-addon">
                     <i class="fa fa-close" id="{{ $addressField }}-address-clear"></i>
                 </span>
@@ -26,7 +26,7 @@ $addressSettings[$addressField] = [
 <div class="row">
     <div class="col-sm-5">
         <div class="form-group {{ $errors->has($addressField . '.city') ? 'has-error' : '' }}">
-            <input type="text" name="{{ $addressField }}[city]" class="form-control" value="{{ $city }}" placeholder="City"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }} />
+            <input type="text" name="{{ $addressField }}[city]" id="{{ $addressField }}-city" class="form-control" value="{{ $city }}" placeholder="City"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }} />
             @if ($errors->has($addressField . '.city'))
                 <span class="help-block">
                     <strong>{{ $errors->first($addressField . '.city') }}</strong>
@@ -36,7 +36,7 @@ $addressSettings[$addressField] = [
     </div>
     <div class="col-sm-3">
         <div class="form-group {{ $errors->has($addressField . '.state_id') ? 'has-error' : '' }}">
-            <select class="form-control" style="width: 100%;" name="{{ $addressField }}[state_id]" id="{{ $addressField }}-state_id" data-placeholder="State"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }}>
+            <select class="form-control" style="width: 100%;" name="{{ $addressField }}[state_id]" id="{{ $addressField }}-state" data-placeholder="State"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }}>
                 <option></option>
                 @foreach($stateList as $state)
                     <option value="{{ $state['id'] }}"{{ $state_id == $state['id'] ? ' selected' : '' }}>{{ $state['iso'] }}</option>
@@ -51,7 +51,7 @@ $addressSettings[$addressField] = [
     </div>
     <div class="col-sm-4">
         <div class="form-group {{ $errors->has($addressField . '.zip') ? 'has-error' : '' }}">
-            <input type="text" name="{{ $addressField }}[zip]" class="form-control" value="{{ $zip }}" placeholder="Zip"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }} />
+            <input type="text" name="{{ $addressField }}[zip]" id="{{ $addressField }}-zip" class="form-control" value="{{ $zip }}" placeholder="Zip"{{ $addressSettings[$addressField]['required'] ? ' required' : '' }} />
             @if ($errors->has($addressField . '.zip'))
                 <span class="help-block">
                     <strong>{{ $errors->first($addressField . '.zip') }}</strong>
@@ -63,6 +63,12 @@ $addressSettings[$addressField] = [
 
 @push('js')
 <script type="text/javascript">
-    $('#{{ $addressField }}-state_id').select2();
+    $('#{{ $addressField }}-state').select2();
+    $('#{{ $addressField }}-address-clear').click(function() {
+        $('#{{ $addressField }}-street').val('');
+        $('#{{ $addressField }}-city').val('');
+        $('#{{ $addressField }}-state').val('').trigger("change");
+        $('#{{ $addressField }}-zip').val('');
+    });
 </script>
 @endpush
