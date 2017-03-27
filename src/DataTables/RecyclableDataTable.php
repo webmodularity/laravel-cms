25902dbcf9d3400bf95d5d16db0237c11099a809
Rory Hellyer <rory@webmodularity.com>
2017-03-27 15:49:23 -0700

    dev

--- src/DataTables/RecyclableDataTable.php
+++ src/DataTables/RecyclableDataTable.php
@@ -2,12 +2,21 @@
 
 namespace WebModularity\LaravelCms\DataTables;
 
+use Carbon\Carbon;
+
 trait RecyclableDataTable
 {
 
     protected function recycleDataTable($dataTable)
     {
-        return $dataTable->onlyTrashed();
+        return $dataTable
+            ->onlyTrashed()
+            ->editColumn('deleted_at', function ($model) {
+                return $model->deleted_at ? with(new Carbon($model->deleted_at))->format('m/d/Y h:i:sa') : null;
+            })
+            ->filterColumn('deleted_at', function ($query, $keyword) {
+                $query->whereRaw("DATE_FORMAT(deleted_at,'%m/%d/%Y %h:%i:%s%p') like ?", ["%$keyword%"]);
+            });
     }
 
 }
