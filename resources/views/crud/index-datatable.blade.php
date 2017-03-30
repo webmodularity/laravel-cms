21656b509646fc76d824f895b327cf9871097841
Rory Hellyer <rory@webmodularity.com>
2017-03-30 01:11:07 -0700

    dev

--- resources/views/crud/index-datatable.blade.php
+++ resources/views/crud/index-datatable.blade.php
@@ -13,7 +13,7 @@
         <div class="col-sm-12">
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">@yield('title')</h3>
+                    <h3 class="box-title">@yield('box-title')</h3>
                     <div class="box-tools pull-right">
 
                     </div><!-- /.box-tools -->
