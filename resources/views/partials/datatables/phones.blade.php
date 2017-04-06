9b6aec596970d849ecf0d17b033b5eed450bd845
Rory Hellyer <rory@webmodularity.com>
2017-04-06 13:40:13 -0700

    dev

--- resources/views/partials/datatables/phones.blade.php
+++ resources/views/partials/datatables/phones.blade.php
@@ -1,6 +1,21 @@
 @foreach($phones as $phone)
-    {!! var_dump($phone) !!}
-    <i class="fa fa-fw fa-phone"></i>
+    @php
+        switch ($phone['pivot']['phone_type_id'])
+        {
+            case \WebModularity\LaravelContact\Phone::TYPE_MOBILE:
+                $phoneIcon = "mobile-phone";
+                break;
+            case \WebModularity\LaravelContact\Phone::TYPE_FAX:
+                $phoneIcon = "fax";
+                break;
+            case \WebModularity\LaravelContact\Phone::TYPE_HOME;
+                $phoneIcon = "home";
+                break;
+            default:
+                $phoneIcon = "phone";
+        }
+    @endphp
+    <i class="fa fa-fw fa-{{ $phoneIcon }}"></i>
     @include('wmcms::partials.phone', ['phone' => $phone])
     @if(!$loop->last)
         <br />
