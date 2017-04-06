69124123f1a9ce9215999e01f7500275b9c4220a
Rory Hellyer <rory@webmodularity.com>
2017-04-06 11:04:35 -0700

    dev

--- resources/views/partials/phone.blade.php
+++ resources/views/partials/phone.blade.php
@@ -1,1 +1,1 @@
-{{ var_dump($phone) }}
+({{ $phone['area_code'] }})&nbsp;{{ substr($phone['number'], 0, 3) }}-{{ substr($phone['number'], 3, 4) }}{{ !empty($phone['extension']) ? "&nbsp;x" . $phone['extension'] : '' }}
