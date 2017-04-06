d7a991c126210a2b92e82b337b994697cd4c827c
Rory Hellyer <rory@webmodularity.com>
2017-04-06 13:03:00 -0700

    dev

--- resources/views/partials/datatables/phones.blade.php
+++ resources/views/partials/datatables/phones.blade.php
@@ -1,1 +1,17 @@
-@each('wmcms::partials.phone', $phones, 'phone')
+<?php
+use WebModularity\LaravelContact\Phone;
+?>
+@foreach($phones as $phone)
+    <i class="fa fa-fw fa-<?php
+    if (Phone::TYPE_MOBILE) {
+        echo "mobile-phone";
+    } elseif (Phone::TYPE_FAX) {
+        echo "fax";
+    } elseif (Phone::TYPE_HOME) {
+        echo "home";
+    } else {
+        echo "phone";
+    }
+    ?>"></i>&nbsp;
+    @include('wmcms::partials.phone', ['phone' => $phone])
+@endforeach
