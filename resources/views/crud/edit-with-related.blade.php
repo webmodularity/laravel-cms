b832a13be5a1226358da7ebe41dd3b1f09dbed99
Rory Hellyer <rory@webmodularity.com>
2017-04-10 21:08:00 -0700

    crud edit template

--- resources/views/crud/edit-with-related.blade.php
+++ resources/views/crud/edit-with-related.blade.php
@@ -52,6 +52,31 @@
             <!-- /.box-body -->
         </div>
     </div>
+    @if(isset($related2TableId))
+        <div class="col-sm-6">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">@yield('related2-box-title') <em>@yield('box-title')</em></h3>
+
+                    <div class="box-tools pull-right">
+
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <table id="{{ $related2TableId }}" class="table table-hover table-bordered">
+                        <thead>
+                        @yield('related2-header-row')
+                        </thead>
+                        <tbody>
+                        @yield('related2-rows')
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
 
@@ -68,6 +93,17 @@
             "autoWidth": true,
             "order": {!! $relatedDefaultOrder or '[[0, "asc"]]' !!}
         });
+        @if(isset($related2TableId))
+        $('#{{ $related2TableId  }}').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": true,
+            "order": {!! $related2DefaultOrder or '[[0, "asc"]]' !!}
+        });
+        @endif
     });
 </script>
 @endpush
