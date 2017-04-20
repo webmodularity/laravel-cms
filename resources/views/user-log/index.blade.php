2274d64fdb77615286e7d331a42a49e0fc3b2400
Rory Hellyer <rory@webmodularity.com>
2017-04-20 12:23:55 -0700

    crud edit template

--- resources/views/user-log/index.blade.php
+++ resources/views/user-log/index.blade.php
@@ -1,29 +1,4 @@
-@extends('wmcms::page')
+@extends('wmcms::crud.index-datatable')
 
 @section('title', 'User Log')
-
-@section('content_header')
-    <h1>User Log</h1>
-@stop
-
-@section('content')
-    <div class="box">
-        <div class="box-header with-border">
-            <h3 class="box-title">User Log</h3>
-
-            <div class="box-tools pull-right">
-
-            </div>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-            {!! $dataTable->table(['class' => "table table-hover table-bordered"]) !!}
-        </div>
-        <!-- /.box-body -->
-    </div>
-@stop
-
-@push('js')
-@dtdefaults()
-{!! $dataTable->scripts() !!}
-@endpush
+@section('box-title', 'User Log')
