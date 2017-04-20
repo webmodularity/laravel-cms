1d9b56940168f006615157e59311dcc132266e54
Rory Hellyer <rory@webmodularity.com>
2017-04-20 13:08:31 -0700

    crud edit template

---
+++ resources/views/crud/actions/details.blade.php
@@ -0,0 +1,3 @@
+<div style="white-space: nowrap">
+    <a href="{{ route(Route::current()->uri() . '.show', ['id' => $id]) }}" class="btn btn-xs btn-primary"><i class="fa fa-eye"></i>&nbsp;Details</a>
+</div>
