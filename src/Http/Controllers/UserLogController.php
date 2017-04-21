32c4e4b35b027bdd410475dd6b041a898a7ad096
Rory Hellyer <rory@webmodularity.com>
2017-04-20 19:46:03 -0700

    crud edit template

--- src/Http/Controllers/UserLogController.php
+++ src/Http/Controllers/UserLogController.php
@@ -20,27 +20,6 @@ class UserLogController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  LogUser  $logUser
@@ -53,39 +32,5 @@ class UserLogController extends Controller
             ->limit(10)
             ->get();
         return view('wmcms::user-log.show')->with('logUser', $logUser)->with('userLogs', $recentUserLogs);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\WebModularity\LaravelUser\LogUser  $logUser
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(LogUser $logUser)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\WebModularity\LaravelUser\LogUser  $logUser
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, LogUser $logUser)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\WebModularity\LaravelUser\LogUser  $logUser
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(LogUser $logUser)
-    {
-        //
     }
 }
