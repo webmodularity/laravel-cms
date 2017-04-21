24f7efa9a1d48755c766b9952feb0ee20a21c429
Rory Hellyer <rory@webmodularity.com>
2017-04-21 11:47:27 -0700

    crud edit template

--- src/DataTables/LogUserDataTable.php
+++ src/DataTables/LogUserDataTable.php
@@ -66,7 +66,7 @@ class LogUserDataTable extends CmsDataTable
             ->leftJoin('user_social_providers', 'log_users.social_provider_id', '=', 'user_social_providers.id')
             ->leftJoin('users', 'log_users.user_id', '=', 'users.id')
             ->leftJoin('people', 'users.person_id', '=', 'people.id')
-            ->with(['socialProvider'])
+            ->with(['socialProvider', 'logRequest.ipAddress'])
             ->orderBy('log_users.id', 'desc');
 
         return $this->applyScopes($query);
