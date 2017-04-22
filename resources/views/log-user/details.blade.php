eeb9c57b3057c84c0e9c30047e6d50758ee59908
Rory Hellyer <rory@webmodularity.com>
2017-04-21 18:17:02 -0700

    details

--- resources/views/log-user/details.blade.php
+++ resources/views/log-user/details.blade.php
@@ -1,6 +1,24 @@
 <div class="form-group">
+    <label class="col-sm-2 control-label">Time</label>
+    <div class="col-sm-10">
+        <p class="form-control-static">{{ $logUser->created_at->format('m/d/Y h:i:sa') }}</p>
+    </div>
+</div>
+<div class="form-group">
+    <label class="col-sm-2 control-label">Action</label>
+    <div class="col-sm-10">
+        <p class="form-control-static">{{ $logUser->userAction->slug }}</p>
+    </div>
+</div>
+<div class="form-group">
     <label class="col-sm-2 control-label">User</label>
     <div class="col-sm-10">
         <p class="form-control-static">{{ $logUser->user->person->email }}</p>
+    </div>
+</div>
+<div class="form-group">
+    <label class="col-sm-2 control-label">Social Provider</label>
+    <div class="col-sm-10">
+        <p class="form-control-static">{{ !is_null($logUser->socialProvider) ? $logUser->socialProvider->getName() : '<em>' . 'None' . '</em>' }}</p>
     </div>
 </div>
