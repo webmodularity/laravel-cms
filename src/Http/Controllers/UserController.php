ed7e23bd389625c667244f55d506b14362a19545
Rory Hellyer <rory@webmodularity.com>
2017-04-22 16:35:29 -0700

    details

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('users.create', ['recentlyAdded' => User::orderBy('id', 'desc')->limit(10)->get()]);
+        return view('wmcms::users.create', ['recentlyAdded' => User::orderBy('id', 'desc')->limit(10)->get()]);
     }
 
     /**
