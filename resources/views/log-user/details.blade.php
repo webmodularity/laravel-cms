5caa0d267340b0805b2b112ccaf08692426e4cfa
Rory Hellyer <rory@webmodularity.com>
2017-04-21 21:19:49 -0700

    details

--- resources/views/log-user/details.blade.php
+++ resources/views/log-user/details.blade.php
@@ -23,9 +23,27 @@
     </div>
 </div>
 <div class="form-group">
+    <label class="col-sm-2 control-label">Query String</label>
+    <div class="col-sm-10">
+        <p class="form-control-static">{!! !is_null($logUser->logRequest->queryString) ? $logUser->logRequest->queryString->query_string : '<em>' . 'None' . '</em>' !!}</p>
+    </div>
+</div>
+<div class="form-group">
+    <label class="col-sm-2 control-label">Ajax Request</label>
+    <div class="col-sm-10">
+        <p class="form-control-static">{{ $logUser->logRequest->is_ajax ? 'Yes' : 'No' }}</p>
+    </div>
+</div>
+<div class="form-group">
     <label class="col-sm-2 control-label">Social Provider</label>
     <div class="col-sm-10">
         <p class="form-control-static">{!! !is_null($logUser->socialProvider) ? $logUser->socialProvider->getName() : '<em>' . 'None' . '</em>' !!}</p>
+    </div>
+</div>
+<div class="form-group">
+    <label class="col-sm-2 control-label">Session ID</label>
+    <div class="col-sm-10">
+        <p class="form-control-static">{{ $logUser->logRequest->session_id }}</p>
     </div>
 </div>
 <div class="form-group">
