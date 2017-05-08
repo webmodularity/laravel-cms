4ce546f54d4eedef04b5adb0a50051d16a54ff9b
Rory Hellyer <rory@webmodularity.com>
2017-05-08 13:08:21 -0700

    dev

--- resources/views/crud/related-box.blade.php
+++ resources/views/crud/related-box.blade.php
@@ -9,10 +9,10 @@
     <div class="box-body">
         <table id="{{ $relatedTableId or 'related-table' }}" class="table table-hover table-bordered">
             <thead>
-            @yield($relatedTableId or 'related-table' . '-header')
+            @yield($relatedTableId . '-header')
             </thead>
             <tbody>
-            @yield($relatedTableId or 'related-table' . '-rows')
+            @yield($relatedTableId . '-rows')
             </tbody>
         </table>
     </div>
