308c395563bb81042424915d977f11a3d70a0bb7
Rory Hellyer <rory@webmodularity.com>
2017-05-09 16:26:39 -0700

    dev

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -35,7 +35,7 @@
                 <div class="box-header with-border">
                     <h3 class="box-title">Social Logins: <em>{{ $user->person->email }}</em></h3>
                     <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-success btn-xs" data-toggle="modal" data-target="#addSocialLogin"><span class="fa fa-plus"></span>&nbsp;Add<span class="hidden-xs hidden-sm"> Social Login</span></button>
+                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#addSocialLogin"><span class="fa fa-plus"></span>&nbsp;Add<span class="hidden-xs hidden-sm"> Social Login</span></button>
                     </div>
                 </div>
                 <!-- /.box-header -->
