a108e0d121955d9e1e946c3eac14706ea44d143d
Rory Hellyer <rory@webmodularity.com>
2017-05-08 23:26:48 -0700

    dev

---
+++ resources/views/crud/recycle-datatable-box.blade.php
@@ -0,0 +1,13 @@
+<div class="box box-danger">
+    <div class="box-header with-border">
+        <h3 class="box-title">{!! $boxTitle !!}</h3>
+        <div class="box-tools pull-right">
+            @yield('box-tools')
+        </div>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        {!! $dataTable->table(['class' => "table table-hover table-bordered"]) !!}
+    </div>
+    <!-- /.box-body -->
+</div>
---
+++ resources/views/crud/recycle-warning.blade.php
@@ -0,0 +1,4 @@
+<div class="callout callout-danger">
+    <h4>Recycle Bin</h4>
+    <p>These records have been soft-deleted and DO NOT show up in results. There is no need to remove these records from the recycle bin unless you have a specific need to do so.</p>
+</div>
