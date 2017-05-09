c0aed4d75130da94f4290171d4e020d4a0d0e9a1
Rory Hellyer <rory@webmodularity.com>
2017-05-08 23:06:13 -0700

    dev

--- resources/views/log-user/index.blade.php
+++ resources/views/log-user/index.blade.php
@@ -1,4 +1,25 @@
-@extends('wmcms::crud.index-datatable')
+@extends('wmcms::page')
 
 @section('title', 'User Log')
-@section('box-title', 'User Log')
+@section('header-title')
+    <h1>User Log</h1>
+@endsection
+
+@section('breadcrumbs')
+    <li class="active">User Log</li>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            @include('wmcms::crud.datatable-box', [
+                'boxTitle' => 'User Log'
+            ])
+        </div>
+    </div>
+@stop
+
+@push('js')
+@dtdefaults()
+{!! $dataTable->scripts() !!}
+@endpush
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,25 @@
-@extends('wmcms::crud.index-datatable')
+@extends('wmcms::page')
 
 @section('title', 'Users')
-@section('box-title', 'Users')
+@section('header-title')
+    <h1>Users<small>Active Records</small></h1>
+@endsection
+
+@section('breadcrumbs')
+    <li class="active">Users</li>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            @include('wmcms::crud.datatable-box', [
+                'boxTitle' => 'Users'
+            ])
+        </div>
+    </div>
+@stop
+
+@push('js')
+@dtdefaults()
+{!! $dataTable->scripts() !!}
+@endpush
