5c30b2411706dd92bc425335ba1c6d3fccbcd345
Rory Hellyer <rory@webmodularity.com>
2017-05-14 21:26:16 -0700

    dev

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -47,7 +47,7 @@
         </div>
     </div>
 
-    <div class="modal fade" tabindex="-1" role="dialog">
+    <div id="addSocialLogin" class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
