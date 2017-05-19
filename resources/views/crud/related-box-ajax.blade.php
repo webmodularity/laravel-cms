001a9707e5244f358412241e75f6e76e852fd766
Rory Hellyer <rory@webmodularity.com>
2017-05-19 11:36:15 -0700

    dev

--- resources/views/crud/related-box-ajax.blade.php
+++ resources/views/crud/related-box-ajax.blade.php
@@ -65,7 +65,7 @@ $order = isset($order) && !empty($order)
             "autoWidth": true,
             "order": {!! $order !!},
             "drawCallback": function (settings) {
-                $("#{{ $relatedAjaxTableId }}Modal").find(".delete-confirm-button").click(function(){
+                $("#{{ $relatedAjaxTableId }}Table").find(".delete-confirm-button").click(function(){
                     var id = $(this).data("id");
                     var token = $(this).data("token");
                     var recordIdent = $(this).data("record-ident");
