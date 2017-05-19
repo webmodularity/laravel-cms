304b71d29cc2e2357c0c482deae0b7c70f2e10f4
Rory Hellyer <rory@webmodularity.com>
2017-05-18 23:34:36 -0700

    dev

---
+++ resources/views/crud/related-box-ajax.blade.php
@@ -0,0 +1,149 @@
+<?php
+$relatedAjaxTableId = isset($relatedAjaxTableId) && !empty($relatedAjaxTableId)
+    ? $relatedAjaxTableId
+    : 'relatedAjaxTable';
+?>
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">{!! $boxTitle !!}</h3>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#add{{ $relatedAjaxTableId }}"><span class="fa fa-plus"></span>&nbsp;{{ $addText }}</button>
+        </div>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        <table id="{{ $relatedAjaxTableId }}Table" class="table table-hover table-bordered"></table>
+    </div>
+    <!-- /.box-body -->
+</div>
+
+@section($relatedAjaxTableId . 'Order', '[[0, "asc"]]')
+<div id="add{{ $relatedAjaxTableId }}" class="modal fade" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">{{ $addText }}</h4>
+            </div>
+            <div class="modal-body">
+                <form id="add{{ $relatedAjaxTableId }}Form">
+                    {!! csrf_field() !!}
+                    @yield($relatedAjaxTableId . 'Form')
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" id="add{{ $relatedAjaxTableId }}Submit">{{ $addText }}</button>
+                </form>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+@push('js')
+<script>
+    $(function () {
+        var {{ $relatedAjaxTableId }}Data = [
+            @yield($relatedAjaxTableId . 'Data')
+        ];
+        var {{ $relatedAjaxTableId }}DataTable = $('#{{ $relatedAjaxTableId }}Table').DataTable({
+            data: {{ $relatedAjaxTableId }}Data,
+            columns: [
+                @yield($relatedAjaxTableId . 'Columns')
+            ],
+            columnDefs: [
+                @yield($relatedAjaxTableId . 'ColumnDefs')
+            ],
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": true,
+            "order": @yield($relatedAjaxTableId . 'Order'),
+            "drawCallback": function (settings) {
+                $('.{{ $relatedAjaxTableId }}-delete-confirm-button').click(function(){
+                    var id = $(this).data("id");
+                    var token = $(this).data("token");
+                    var recordIdent = $(this).data("record-ident");
+                    var row = $(this).parents('tr');
+                    swal({
+                            title: 'Delete This Record?',
+                            text: recordIdent,
+                            type: 'warning',
+                            showCancelButton: true,
+                            confirmButtonClass: 'btn-danger',
+                            confirmButtonText: 'Yes, delete it!',
+                            closeOnConfirm: false,
+                            showLoaderOnConfirm: true
+                        },
+                        function() {
+                            $.ajax({
+                                url: @yield($relatedAjaxTableId . 'DeleteUrl'),
+                                method: 'POST',
+                                data: {
+                                    '_method': 'DELETE',
+                                    '_token': token,
+                                },
+                                success: function (response) {
+                                    {{ $relatedAjaxTableId }}DataTable.row(row).remove().draw();
+                                    toastr.success(response);
+                                    swal.close();
+                                },
+                                error: function (xhr, status, error) {
+                                    toastr.error(JSON.parse(xhr.responseText) || 'An unknown server error was encountered when attempting to delete this record.');
+                                    swal.close();
+                                }
+                            });
+                        });
+                });
+            }
+        });
+
+        $('#{{ $relatedAjaxTableId }}Form').on('submit', function(event) {
+            event.preventDefault();
+            var form = $(this);
+            var submitButton = $("#add{{ $relatedAjaxTableId }}Submit");
+            var formGroups = form.find("div.form-group");
+            formGroups.removeClass('has-error');
+            formGroups.find("span.help-block").remove();
+            var submitHtmlOrig = submitButton.html();
+            submitButton.html('<i class="fa fa-spinner fa-pulse"></i>&nbsp;Saving... <span class="sr-only">Saving...</span>');
+            submitButton.prop('disabled', true);
+            $.ajax({
+                type:"POST",
+                url:'@yield($relatedAjaxTableId . 'PostUrl')',
+                data:$(this).serialize(),
+                dataType: 'json',
+                success: function(data) {
+                    toastr.success(data);
+                    submitButton.html('<i class="fa fa-check"></i>&nbsp;Saved<span class="sr-only">Saved</span>');
+                    submitButton.removeClass("btn-primary").addClass("btn-success");
+                    @yield($relatedAjaxTableId . 'RowAddData')
+                    {{ $relatedAjaxTableId }}DataTable.row.add(rowAddData).draw();
+                    setTimeout(function() {
+                        submitButton.html(submitHtmlOrig);
+                        submitButton.removeClass("btn-success").addClass("btn-primary");
+                        submitButton.prop('disabled', false);
+                        form.trigger("reset");
+                        $("#add{{ $relatedAjaxTableId }}").modal('hide');
+                    }, 1000);
+                },
+                error: function(data) {
+                    toastr.error("An error occurred while saving. Please correct the errors and resubmit.");
+                    submitButton.html(submitHtmlOrig);
+                    submitButton.prop('disabled', false);
+                    $.each(data.responseJSON, function (index, value) {
+                        console.log(index);
+                        console.log(value);
+                        var errorFormGroup = form.find(":input[name='"+index+"']").parent('div.form-group');
+                        errorFormGroup.addClass('has-error');
+                        errorFormGroup.append("<span class=\"help-block\">"+value+"</span>");
+                    });
+                }
+            })
+        });
+    });
+</script>
+@endpush
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -31,206 +31,92 @@
             ])
         </div>
         <div class="col-sm-6">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Social Logins: <em>{{ $user->person->email }}</em></h3>
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addSocialLogin"><span class="fa fa-plus"></span>&nbsp;Add<span class="hidden-xs hidden-sm"> Social Login</span></button>
-                    </div>
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body">
-                    <table id="userSocialLoginTable" class="table table-hover table-bordered"></table>
-                </div>
-                <!-- /.box-body -->
-            </div>
+            @include('wmcms::crud.related-box-ajax', [
+                'boxTitle' => "Social Logins: <em>".$user->person->email."</em>",
+                'addText' => "Add Social Login",
+                'relatedAjaxTableId' => 'userSocialLogin'
+            ])
         </div>
-    </div>
-
-    <div id="addSocialLogin" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title">Add Social Login</h4>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('users.social.attach', ['user_id' => $user->id]) }}" method="post" id="addUserSocialLoginForm">
-                        {!! csrf_field() !!}
-                        <div class="form-group">
-                            <label class="control-label">User</label>
-                            <p>{{ $user->person->email }}</p>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="social_provider_id">Social Provider</label>
-                            <select class="form-control" style="width: 100%;" name="social_provider_id" id="social_provider_id" required>
-                                @foreach($socialProviders as $socialProvider)
-                                    <option value="{{ $socialProvider['id'] }}"{{ old('social_provider_id') == $socialProvider['id'] ? ' selected' : '' }}>{{ $socialProvider->getName() }}</option>
-                                @endforeach()
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="uid">Social User ID</label>
-                            <input type="text" name="uid" class="form-control" placeholder="Social User ID" required />
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="email">Social User Email</label>
-                            <input type="email" name="email" class="form-control" placeholder="Email Address" required />
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="avatar_url">Avatar URL (Optional)</label>
-                            <input type="url" name="avatar_url" class="form-control" placeholder="Avatar URL" />
-                        </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary" id="addUserSocialLoginSubmit">Add Social Login</button>
-                    </form>
-                </div>
-            </div>
-            <!-- /.modal-content -->
-        </div>
-        <!-- /.modal-dialog -->
     </div>
 @endsection
 
-@push('js')
-<script>
-    $(function () {
-        var userSocialLoginData = [
-            @foreach($user->socialProviders as $socialProvider)
-            [
-                "{{ $socialProvider->id }}",
-                "{{ $socialProvider->getName() }}",
-                "{{ $socialProvider->pivot->avatar_url }}",
-                "{{ $socialProvider->pivot->uid }}",
-                "{{ $socialProvider->pivot->email }}"
-            ]
-            @endforeach
-        ];
-        var userSocialLoginDataTable = $('#userSocialLoginTable').DataTable({
-            data: userSocialLoginData,
-            columns: [
-                { visible: false },
-                { title: "Social" },
-                { title: "Avatar", orderable: false, searchable: false },
-                { title: "User ID" },
-                { title: "Email" },
-                { title: "Delete", orderable: false, searchable: false },
-            ],
-            columnDefs: [
-                {
-                    render: function (data, type, row) {
-                        if (data) {
-                            return '<img src="' + data + '" width="40" height="40" title="' + data + '" />';
-                        } else {
-                            return '<em>None</em>';
-                        }
-                    },
-                    targets: 2
-                },
-                {
-                    render: function (data, type, row) {
-                        return '<button type="button" class="btn btn-xs btn-danger delete-confirm-button" data-id="'+row[0]+'" data-token="{{ csrf_token() }}" data-record-ident="'+row[1]+'"><i class="fa fa-trash-o"></i>&nbsp;Delete</button>';
-                    },
-                    width: "40px",
-                    targets: 5
-                }
-            ],
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": true,
-            "order": [[0, "asc"]],
-            "drawCallback": function (settings) {
-                $('.delete-confirm-button').click(function(){
-                    var id = $(this).data("id");
-                    var token = $(this).data("token");
-                    var recordIdent = $(this).data("record-ident");
-                    var row = $(this).parents('tr');
-                    swal({
-                            title: 'Delete This Record?',
-                            text: recordIdent,
-                            type: 'warning',
-                            showCancelButton: true,
-                            confirmButtonClass: 'btn-danger',
-                            confirmButtonText: 'Yes, delete it!',
-                            closeOnConfirm: false,
-                            showLoaderOnConfirm: true
-                        },
-                        function() {
-                            $.ajax({
-                                url: location.pathname.replace(/\/?$/, '') + '/social-logins/' +id,
-                                method: 'POST',
-                                data: {
-                                    '_method': 'DELETE',
-                                    '_token': token,
-                                },
-                                success: function (response) {
-                                    userSocialLoginDataTable.row(row).remove().draw();
-                                    toastr.success(response);
-                                    swal.close();
-                                },
-                                error: function (xhr, status, error) {
-                                    toastr.error(JSON.parse(xhr.responseText) || 'An unknown server error was encountered when attempting to delete this record.');
-                                    swal.close();
-                                }
-                            });
-                        });
-                });
-            }
-        });
+@section('userSocialLoginDeleteUrl', "location.pathname.replace(/\/?$/, '') + '/social-logins/' +id")
+@section('userSocialLoginPostUrl', route('users.social.attach', ['user_id' => $user->id]))
+@section('userSocialLoginData')
+    @foreach($user->socialProviders as $socialProvider)
+        [
+        "{{ $socialProvider->id }}",
+        "{{ $socialProvider->getName() }}",
+        "{{ $socialProvider->pivot->avatar_url }}",
+        "{{ $socialProvider->pivot->uid }}",
+        "{{ $socialProvider->pivot->email }}"
+        ]
+    @endforeach
+@endsection
 
-        $('#addUserSocialLoginForm').on('submit', function(event) {
-            event.preventDefault();
-            var form = $(this);
-            var submitButton = $("#addUserSocialLoginSubmit");
-            var formGroups = form.find("div.form-group");
-            formGroups.removeClass('has-error');
-            formGroups.find("span.help-block").remove();
-            var submitHtmlOrig = submitButton.html();
-            submitButton.html('<i class="fa fa-spinner fa-pulse"></i>&nbsp;Saving... <span class="sr-only">Saving...</span>');
-            submitButton.prop('disabled', true);
-            $.ajax({
-                type:"POST",
-                url:'{{ route('users.social.attach', ['user_id' => $user->id]) }}',
-                data:$(this).serialize(),
-                dataType: 'json',
-                success: function(data) {
-                    toastr.success(data);
-                    submitButton.html('<i class="fa fa-check"></i>&nbsp;Saved<span class="sr-only">Saved</span>');
-                    submitButton.removeClass("btn-primary").addClass("btn-success");
-                    var socialProviderSelected = form.find("#social_provider_id option:checked");
-                    var socialProviderId = socialProviderSelected.val();
-                    var socialProviderName = socialProviderSelected.text();
-                    var avatarUrlVal = form.find("input[name=avatar_url]").val();
-                    var uidVal = form.find("input[name=uid]").val();
-                    var emailVal = form.find("input[name=email]").val();
-                    userSocialLoginDataTable.row.add([socialProviderId, socialProviderName, avatarUrlVal, uidVal, emailVal]).draw();
-                    setTimeout(function() {
-                        submitButton.html(submitHtmlOrig);
-                        submitButton.removeClass("btn-success").addClass("btn-primary");
-                        submitButton.prop('disabled', false);
-                        form.trigger("reset");
-                        $("#addSocialLogin").modal('hide');
-                    }, 1000);
-                },
-                error: function(data) {
-                    toastr.error("An error occurred while saving. Please correct the errors and resubmit.");
-                    submitButton.html(submitHtmlOrig);
-                    submitButton.prop('disabled', false);
-                    $.each(data.responseJSON, function (index, value) {
-                        console.log(index);
-                        console.log(value);
-                        var errorFormGroup = form.find(":input[name='"+index+"']").parent('div.form-group');
-                        errorFormGroup.addClass('has-error');
-                        errorFormGroup.append("<span class=\"help-block\">"+value+"</span>");
-                    });
-                }
-            })
-        });
-    });
-</script>
-@endpush
+@section('userSocialLoginColumns')
+    { visible: false },
+    { title: "Social" },
+    { title: "Avatar", orderable: false, searchable: false },
+    { title: "User ID" },
+    { title: "Email" },
+    { title: "Delete", orderable: false, searchable: false },
+@endsection
+
+@section('userSocialLoginColumnDefs')
+    {
+    render: function (data, type, row) {
+    if (data) {
+    return '<img src="' + data + '" width="40" height="40" title="' + data + '" />';
+    } else {
+    return '<em>None</em>';
+    }
+    },
+    targets: 2
+    },
+    {
+    render: function (data, type, row) {
+    return '<button type="button" class="btn btn-xs btn-danger delete-confirm-button" data-id="'+row[0]+'" data-token="{{ csrf_token() }}" data-record-ident="'+row[1]+'"><i class="fa fa-trash-o"></i>&nbsp;Delete</button>';
+    },
+    width: "40px",
+    targets: 5
+    }
+@endsection
+
+@section('userSocialLoginRowAddData')
+    var socialProviderSelected = form.find("#social_provider_id option:checked");
+    var rowAddData = [
+        socialProviderSelected.val(),
+        socialProviderSelected.text(),
+        form.find("input[name=avatar_url]").val(),
+        form.find("input[name=uid]").val(),
+        form.find("input[name=email]").val()
+    ];
+@endsection
+
+@section('userSocialLoginForm')
+    <div class="form-group">
+        <label class="control-label">User</label>
+        <p>{{ $user->person->email }}</p>
+    </div>
+    <div class="form-group">
+        <label class="control-label" for="social_provider_id">Social Provider</label>
+        <select class="form-control" style="width: 100%;" name="social_provider_id" id="social_provider_id" required>
+            @foreach($socialProviders as $socialProvider)
+                <option value="{{ $socialProvider['id'] }}"{{ old('social_provider_id') == $socialProvider['id'] ? ' selected' : '' }}>{{ $socialProvider->getName() }}</option>
+            @endforeach()
+        </select>
+    </div>
+    <div class="form-group">
+        <label class="control-label" for="uid">Social User ID</label>
+        <input type="text" name="uid" class="form-control" placeholder="Social User ID" required />
+    </div>
+    <div class="form-group">
+        <label class="control-label" for="email">Social User Email</label>
+        <input type="email" name="email" class="form-control" placeholder="Email Address" required />
+    </div>
+    <div class="form-group">
+        <label class="control-label" for="avatar_url">Avatar URL (Optional)</label>
+        <input type="url" name="avatar_url" class="form-control" placeholder="Avatar URL" />
+    </div>
+@endsection
