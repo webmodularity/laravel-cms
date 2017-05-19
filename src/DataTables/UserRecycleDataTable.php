032c4d087c434d8284229295314d0a5e98f15685
Rory Hellyer <rory@webmodularity.com>
2017-05-19 13:12:05 -0700

    dev

--- src/DataTables/UserRecycleDataTable.php
+++ src/DataTables/UserRecycleDataTable.php
@@ -8,6 +8,18 @@ class UserRecycleDataTable extends UserDataTable
 
     protected $actionView = 'users.actions.recycle';
 
+    protected function recycleDataTable($dataTable)
+    {
+        return $dataTable
+            ->onlyTrashed()
+            ->editColumn('deleted_at', function ($model) {
+                return $model->deleted_at ? with(new Carbon($model->deleted_at))->format('m/d/Y h:i:sa') : null;
+            })
+            ->filterColumn('deleted_at', function ($query, $keyword) {
+                $query->whereRaw("DATE_FORMAT(users.deleted_at,'%m/%d/%Y %h:%i:%s%p') like ?", ["%$keyword%"]);
+            });
+    }
+
     protected function getOrder()
     {
         return [[6, "desc"]];
