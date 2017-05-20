a1a8f9affc248ff092746303c1d70229ffc87787
Rory Hellyer <rory@webmodularity.com>
2017-05-20 16:27:46 -0700

    dev

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -22,6 +22,29 @@
             ])
 @endsection
 
+@section('related-table-header')
+    <tr>
+        <th>ID</th>
+        <th>Time</th>
+        <th>IP</th>
+        <th>Action</th>
+    </tr>
+@endsection
+
+@section('related-table-rows')
+    @foreach($userLogs as $userLog)
+        <tr>
+            <td><a href="{{ route('log-user.show', ['id' => $userLog->id]) }}">{{ $userLog->id }}</a></td>
+            <td data-order="{{ $userLog->created_at->format('U') }}"
+                data-sort="{{ $userLog->created_at->format('U') }}">
+                {{ $userLog->created_at->format('m/d/Y h:i:sa') }}
+            </td>
+            <td>{{ $userLog->logRequest->ipAddress->ip }}</td>
+            <td>{{ $userLog->userAction->slug }}</td>
+        </tr>
+    @endforeach
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-sm-6">
@@ -32,6 +55,10 @@
         </div>
         <div class="col-sm-6">
             @include('wmcms::users.edit-social-logins')
+            @include('wmcms::crud.related-box', [
+                'boxTitle' => 'Recent User Activity: <em>' . $user->person->email . '</em>',
+                'defaultOrder' => '[[0, "desc"]]'
+            ])
         </div>
     </div>
 @endsection
