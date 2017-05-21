0c72bb9b0fece596759fbf2987505591ca366d35
Rory Hellyer <rory@webmodularity.com>
2017-05-21 00:15:31 -0700

    dev

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -43,7 +43,7 @@
 @section('userLogColumnDefs')
     {
         render: function (data, type, row) {
-            return '<a href="{{ route('log-user.index') }}/'+data+'" class="btn btn-xs btn-primary"><i class="fa fa-eye"></i></a>';
+            return '<a href="{{ route('log-user.index') }}/'+data+'" class="btn btn-xs btn-primary"><i class="fa fa-info-circle"></i></a>';
         },
         width: "20px",
         targets: 3
