2d6357e94e732879613c9d45f277c6f4ba4760f3
Rory Hellyer <rory@webmodularity.com>
2017-05-22 02:17:30 -0700

    dev

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -41,8 +41,8 @@
 
 @yield('body')
 
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('vendor/adminlte/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ mix('/js/app.js') }}"></script>
+<script src="{{ mix('/js/bootstrap.min.js') }}"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.min.js"></script>
