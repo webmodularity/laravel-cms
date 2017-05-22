e23c899261640d7ad0e64f02f5602b097bb44a7f
Rory Hellyer <rory@webmodularity.com>
2017-05-21 18:18:20 -0700

    dev

--- resources/views/log-user/show-modal.blade.php
+++ resources/views/log-user/show-modal.blade.php
@@ -2,67 +2,67 @@
     <div class="form-group">
         <label class="col-sm-2 control-label">Time</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogCreatedAt">{{ $logUser->created_at->format('m/d/Y h:i:sa') }}</p>
+            <p class="form-control-static" id="userLogCreatedAt"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">Action</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogUserAction">{{ $logUser->userAction->slug }}</p>
+            <p class="form-control-static" id="userLogUserAction"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">Request Method</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogRequestMethod">{{ $logUser->logRequest->requestMethod->method }}</p>
+            <p class="form-control-static" id="userLogRequestMethod"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">URL</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogUrlPath">{{ $logUser->logRequest->urlPath->url_path }}</p>
+            <p class="form-control-static" id="userLogUrlPath"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">Query String</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogQueryString" style="-ms-word-break: break-all; word-break: break-all;">{!! !is_null($logUser->logRequest->queryString) ? $logUser->logRequest->queryString->query_string : '<em>' . 'None' . '</em>' !!}</p>
+            <p class="form-control-static" id="userLogQueryString" style="-ms-word-break: break-all; word-break: break-all;"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">Ajax Request</label>
         <div class="col-sm-10">
-            <p class="form-control-static">{{ $logUser->logRequest->is_ajax ? 'Yes' : 'No' }}</p>
+            <p class="form-control-static"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">Social Provider</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogSocialProvider">{!! !is_null($logUser->socialProvider) ? $logUser->socialProvider->getName() : '<em>' . 'None' . '</em>' !!}</p>
+            <p class="form-control-static" id="userLogSocialProvider"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">Session ID</label>
         <div class="col-sm-10">
-            <p class="form-control-static" style="-ms-word-break: break-all; word-break: break-all; word-break: break-word;">{{ $logUser->logRequest->session_id }}</p>
+            <p class="form-control-static" style="-ms-word-break: break-all; word-break: break-all; word-break: break-word;"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">IP Address</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogIpAddress">{!! !is_null($logUser->logRequest->ipAddress) ? $logUser->logRequest->ipAddress->ip : '<em>' . 'None' . '</em>' !!}</p>
+            <p class="form-control-static" id="userLogIpAddress"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">User</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogUser">{{ $logUser->user->person->email }}</p>
+            <p class="form-control-static" id="userLogUser"></p>
         </div>
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">User Agent</label>
         <div class="col-sm-10">
-            <p class="form-control-static" id="userLogUserAgent" style="-ms-word-break: break-all; word-break: break-all; word-break: break-word;">{{ $logUser->logRequest->userAgent->user_agent }}</p>
+            <p class="form-control-static" id="userLogUserAgent" style="-ms-word-break: break-all; word-break: break-all; word-break: break-word;"></p>
         </div>
     </div>
 @endsection
