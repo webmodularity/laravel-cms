0511bd337d3be129e26807f5f49b031bdc0f697f
Rory Hellyer <rory@webmodularity.com>
2017-05-29 16:38:05 -0700

    dev

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -84,7 +84,7 @@
 @section('adminlte_js')
     <script>
         $(function () {
-            $('input').icheck({
+            $('input').iCheck({
                 checkboxClass: 'icheckbox_square-blue',
                 radioClass: 'iradio_square-blue',
                 increaseArea: '20%' // optional
