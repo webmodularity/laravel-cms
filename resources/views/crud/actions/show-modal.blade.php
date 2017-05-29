a6769072740aeb6c2277c12480fe3f090cf3b1ee
Rory Hellyer <rory@webmodularity.com>
2017-05-29 03:45:10 -0700

    dev

--- resources/views/crud/actions/show-modal.blade.php
+++ resources/views/crud/actions/show-modal.blade.php
@@ -1,3 +1,3 @@
 <div style="white-space: nowrap">
-    <a href="{{ route(preg_replace("/\.".Route::current()->getActionMethod()."$/", '.show', Route::current()->getName()), ['id' => $id]) }}" class="btn btn-xs btn-primary"><i class="fa fa-search-plus"></i></a>
+    <a href="{{ route(preg_replace("/\.".Route::current()->getActionMethod()."$/", '.show', Route::current()->getName()), ['id' => $id]) }}" data-toggle="modal" data-target="#userLogModal" data-id="{{ $id }}" class="btn btn-xs btn-primary"><i class="fa fa-search-plus"></i></a>
 </div>
