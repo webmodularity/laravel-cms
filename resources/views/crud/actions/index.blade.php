1d439854e5c5a19cf4dd7829c57c81bb25f82baa
Rory Hellyer <rory@webmodularity.com>
2017-05-30 00:05:49 -0700

    dev

--- resources/views/crud/actions/index.blade.php
+++ resources/views/crud/actions/index.blade.php
@@ -1,4 +1,4 @@
 <div style="white-space: nowrap">
-    <a href="{{ route(Route::current()->uri() . '.edit', ['id' => $id]) }}" class="btn btn-xs btn-primary"><i class="fa fa-edit"></i>&nbsp;Edit</a>
-    <button type="button" class="btn btn-xs btn-danger delete-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $recordIdent }}"><i class="fa fa-trash-o"></i>&nbsp;Delete</button>
+    <a href="{{ route(preg_replace("/\.".Route::current()->getActionMethod()."$/", '.edit', Route::current()->getName()), ['id' => $id]) }}" class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i></a>
+    <button type="button" class="btn btn-xs btn-danger delete-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $recordIdent }}"><i class="fa fa-trash-o"></i></button>
 </div>
