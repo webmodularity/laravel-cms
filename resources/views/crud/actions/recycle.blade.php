832394c906a384139c0f6d0364f00bd9ee4bf8f1
Rory Hellyer <rory@webmodularity.com>
2017-05-31 12:19:59 -0700

    dev

--- resources/views/crud/actions/recycle.blade.php
+++ resources/views/crud/actions/recycle.blade.php
@@ -1,4 +1,4 @@
 <div style="white-space: nowrap">
-    <button type="button" class="btn btn-xs btn-success restore-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $recordIdent }}"><i class="fa fa-undo"></i>&nbsp;Restore</button>
-    <button type="button" class="btn btn-xs btn-danger perma-delete-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $recordIdent }}"><i class="fa fa-trash"></i>&nbsp;Delete</button>
+    <button type="button" class="btn btn-xs btn-success restore-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $recordIdent }}"><i class="fa fa-undo"></i></button>
+    <button type="button" class="btn btn-xs btn-danger perma-delete-confirm-button" data-id="{{ $id }}" data-token="{{ csrf_token() }}" data-record-ident="{{ $recordIdent }}"><i class="fa fa-trash"></i></button>
 </div>
