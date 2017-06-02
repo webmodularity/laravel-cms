9fb604abf4ee3b3eaeeffbc02743cf39d6e3c8dc
Rory Hellyer <rory@webmodularity.com>
2017-06-02 02:28:13 -0700

    dev

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -21,7 +21,7 @@ class Controller extends BaseController
     protected function sendJsonFailureResponse($error = null)
     {
         \Log::warning('Failure JSON:' . $error);
-        return response()->json($error, 422);
+        return response()->json(['error' => $error], 422);
     }
 
     protected function sendJsonSuccessResponse($successMessage = '')
