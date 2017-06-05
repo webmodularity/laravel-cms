26833c7a16bbc99d0c1b4ba328b966a618080f26
Rory Hellyer <rory@webmodularity.com>
2017-06-05 03:58:47 -0700

    dev

--- resources/views/partials/name-full.blade.php
+++ resources/views/partials/name-full.blade.php
@@ -1,3 +1,1 @@
-@if(isset($person) && !empty($person['first_name']))
-{{ $person['first_name'] }} {{ !empty($person['middle_name']) ? $person['middle_name'] . ' ' : '' }} {{ $person['last_name'] }}
-@endif
+{{ isset($person) && !empty($person['first_name']) ? $person['first_name'] . ' ' . (!empty($person['middle_name']) ? $person['middle_name'] . ' ' : '') . ' ' . $person['last_name'] : '' }}
