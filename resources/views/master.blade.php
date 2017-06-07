40234a5baa467dbac0e62953ea5d1fb1c5b5f109
Rory Hellyer <rory@webmodularity.com>
2017-06-07 16:59:51 -0700

    dev

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,8 +9,6 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ mix('css/AdminLTE.css') }}" />
     <link rel="stylesheet" href="{{ mix('css/dt.css') }}" />
     @stack('css')
     @yield('css')
