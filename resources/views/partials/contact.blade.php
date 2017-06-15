c093ee8f6b39202863563d0e7afed032a884baed
Rory Hellyer <rory@webmodularity.com>
2017-06-14 19:27:27 -0700

    dev

---
+++ resources/views/partials/contact.blade.php
@@ -0,0 +1,8 @@
+@if(!empty($person['first_name']))
+<div>
+    <div>@include('wmcms::partials.name-full', ['person' => $person])</div>
+    <div><span class="text-muted">{{ $person['email'] }}</span></div>
+</div>
+@else
+    @include('wmcms::partials.name-full', ['person' => $person])
+@endif
