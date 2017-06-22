164434e568997e40bbd0b01461faedd27a6994d8
Rory Hellyer <rory@webmodularity.com>
2017-06-22 16:47:35 -0700

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -125,18 +125,23 @@ class CmsServiceProvider extends ServiceProvider
     $.extend(true, $.fn.dataTable.defaults, {
         buttons: [],
         language: {
+            search: '<div class="input-group" style="width: 100%">' +
+            '<div class="input-group-btn">' +
+            '<button id="datepicker-updated_at" type="button" class="btn btn-sm btn-primary" title="Updated At"><span class="fa fa-calendar"></span>&nbsp;<span class="fa fa-caret-down"></span></button>' +
+            '<button id="filter" type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-search"></span>&nbsp;<span class="fa fa-caret-down"></span></button>' +
+            '<ul class="dropdown-menu dropdown-menu-right"><li><a href="javascript:void(0);" id="dataTableSearchClear"><span class="fa fa-times"></span>&nbsp;Clear Search</a></li></ul></div></div>',
             lengthMenu: '<div class="btn-group" role="group">' +
             '<button type="button" class="btn btn-sm btn-default">Results per page:</button>' +
             '_MENU_' +
             '</div>'
         },
-        dom: "<'row'<'col-sm-9'B><'col-sm-3'<'pull-right'l>>>" +
+        dom: "<'row'<'col-sm-8'B><'col-sm-4'<'#dataTableFilter.pull-right'f>>>" +
         "<'row'<'col-sm-12'tr>>" +
-        "<'row'<'col-sm-5'i><'col-sm-7'p>>"
+        "<'row'<'col-sm-5'li><'col-sm-7'p>>"
     });
     
     $.extend($.fn.dataTable.ext.classes, {
-        sFilterInput: "form-control input-sm w-100",
+        sFilterInput: "hidden",
     });
 </script>
 EOT;
