d0c0b4d30d4c5f2f9a42e8ab6977f35cac42d043
Rory Hellyer <rory@webmodularity.com>
2017-06-22 16:27:47 -0700

    dev

--- resources/views/crud/datatable-box.blade.php
+++ resources/views/crud/datatable-box.blade.php
@@ -1,13 +1,16 @@
 <div class="box box-primary">
     <div class="box-header with-border">
         <h3 class="box-title">{!! $boxTitle !!}</h3>
-        <div class="pull-right">
-            @yield('box-tools')
+        <div class="box-tools pull-right">
+
         </div><!-- /.box-tools -->
     </div><!-- /.box-header -->
     <!-- /.box-header -->
     <div class="box-body">
         {!! $dataTable->table(['class' => "table table-hover table-bordered"]) !!}
+        <div class="row">
+            @yield('box-tools')
+        </div>
     </div>
     <!-- /.box-body -->
 </div>
