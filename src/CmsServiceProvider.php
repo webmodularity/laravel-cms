770dd21e2d114518cd757bdd2e3a53f99ac5eac8
Rory Hellyer <rory@webmodularity.com>
2017-06-22 20:40:26 -0700

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -126,7 +126,7 @@ class CmsServiceProvider extends ServiceProvider
         buttons: [],
         language: {
             lengthMenu: '<div class="btn-group" role="group">' +
-                '<button type="button" class="btn btn-sm btn-default" id="dataTableLengthReset" title="Reset to Default" disabled><span class="text-muted">Results per page:</span></button>' +
+                '<button type="button" class="btn btn-sm btn-default" id="dataTableLengthReset" title="Reset to Default"><span class="text-muted">Results per page:</span></button>' +
                 '_MENU_' +
                 '</div>'
         },
