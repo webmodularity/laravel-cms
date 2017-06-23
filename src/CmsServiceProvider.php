53c810babe3a669678628d5f9185549fb2031eb4
Rory Hellyer <rory@webmodularity.com>
2017-06-22 18:01:49 -0700

    dev

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -130,7 +130,7 @@ class CmsServiceProvider extends ServiceProvider
                 '_MENU_' +
                 '</div>'
         },
-        dom: "<'row'<'col-sm-8'B><'col-sm-4','pull-right'l>>>" +
+        dom: "<'row'<'col-sm-8'B><'col-sm-4'<'pull-right'l>>>" +
             "<'row'<'col-sm-12'tr>>" +
             "<'row'<'col-sm-5'i><'col-sm-7'p>>"
     });
