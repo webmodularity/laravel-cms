e7de8ced6dcacf634405e9d35d0f4634ab9a2bfa
Rory Hellyer <rory@webmodularity.com>
2017-06-30 19:49:11 -0700

    dev

--- resources/views/crud/datatable-filter.blade.php
+++ resources/views/crud/datatable-filter.blade.php
@@ -35,9 +35,58 @@ $datepickerColName = isset($datepickerColName) ? $datepickerColName : null;
 </div>
 
 @push('js')
+<script>
     @if($datepickerColName)
-        @include('wmcms::crud.datatable-filter-datetimepicker', [
-            'datepickerColName' => $datepickerColName
-        ])
+            $(function () {
+        $('#datepicker-{{ $datepickerColName }}').daterangepicker({
+            "timePicker": true,
+            "timePickerIncrement": 1,
+            "linkedCalendars": false,
+            "opens": "left",
+            "autoUpdateInput": false,
+            "alwaysShowCalendars": true,
+            "timePickerSeconds": true,
+            "ranges": {
+                "Today": [
+                    moment().startOf('day'),
+                    moment().endOf('day')
+                ],
+                "Yesterday": [
+                    moment().subtract(1, 'days').startOf('day'),
+                    moment().subtract(1, 'days').endOf('day')
+                ],
+                "Last 7 Days": [
+                    moment().subtract(7, 'days').startOf('day'),
+                    moment().endOf('day')
+                ],
+                "Last 30 Days": [
+                    moment().subtract(30, 'days').startOf('day'),
+                    moment().endOf('day')
+                ],
+                "Last 365 Days": [
+                    moment().subtract(365, 'days').startOf('day'),
+                    moment().endOf('day')
+                ],
+                "This Month": [
+                    moment().startOf('month'),
+                    moment().endOf('month')
+                ],
+                "This Year": [
+                    moment().startOf('year'),
+                    moment().endOf('year')
+                ]
+            },
+        }).on('apply.daterangepicker', function(ev, picker) {
+            $('#dataTableSearch').val(function(index, val) {
+                return WMCMS.DT.FILTER.getAllKeywords(val, '{{ $datepickerColName }}').concat(
+                    [
+                        '{{ $datepickerColName }}:>=' + picker.startDate.format('MM/DD/YYYYHHmmss'),
+                        '{{ $datepickerColName }}:<=' + picker.endDate.format('MM/DD/YYYYHHmmss')
+                    ]
+                ).join(" ");
+            }).trigger('keyup');
+        });
+    });
     @endif
+</script>
 @endpush
