1fd4f06d84f8450cee4104149e9891d16568eb28
Rory Hellyer <rory@webmodularity.com>
2017-07-03 16:30:02 -0700

    dev

--- resources/views/crud/datatable-filter-columns.blade.php
+++ resources/views/crud/datatable-filter-columns.blade.php
@@ -13,7 +13,7 @@
         }
         ?>
         @foreach ($columnFilterData as $filterData)
-            <li><a data-column-filter-name="{{ $filterData['name'] }}"{!! array_key_exists('value', $filterData) ? ' data-column-filter-value="' . $filterData['value'] . '"' : '' !!} href="javascript:void(0)">{{ array_key_exists('display', $filterData) ? $filterData['display'] : ucwords(str_replace('_', ' ', $filterData['name'])) }}</a></li>
+            <li><a data-column-filter-name="{{ $filterData['name'] }}"{!! array_key_exists('value', $filterData) ? ' data-column-filter-value="' . $filterData['value'] . '"' : '' !!}{!! array_key_exists('replace', $filterData) && !$filterData['replace'] ? ' data-column-filter-replace="false"' : '' !!} href="javascript:void(0)">{{ array_key_exists('display', $filterData) ? $filterData['display'] : ucwords(str_replace('_', ' ', $filterData['name'])) }}</a></li>
         @endforeach
     @elseif($columnFilter == 'SEPARATOR' || $columnFilter == 'DIVIDER')
         <li role="separator" class="divider"></li>
