0b16f6135c811c68af707b0f50172a6434a407cd
Rory Hellyer <rory@webmodularity.com>
2017-07-17 15:23:13 -0700

    dev

--- resources/views/crud/related-box.blade.php
+++ resources/views/crud/related-box.blade.php
@@ -7,7 +7,7 @@ $relatedTableId = isset($relatedTableId) && !empty($relatedTableId)
 @push('js')
 <script>
     $(function () {
-        var dataTable{{ $relatedTableId }} = $('#{{ $relatedTableId }}').DataTable({
+        var window.dataTable{{ $relatedTableId }} = $('#{{ $relatedTableId }}').DataTable({
             data: [
                 @yield($relatedTableId . 'Data')
             ],
