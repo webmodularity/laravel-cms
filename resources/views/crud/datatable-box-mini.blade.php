2f8aa841afc5ed56561767c2a3ee724725926a8e
Rory Hellyer <rory@webmodularity.com>
2017-07-17 14:18:17 -0700

    dev

--- resources/views/crud/datatable-box-mini.blade.php
+++ resources/views/crud/datatable-box-mini.blade.php
@@ -19,31 +19,15 @@ $tableId = isset($tableId) && !empty($tableId)
 
 @push('js')
 <script>
-    $(function () {
-        $.fn.dataTable.moment('m/d/Y h:i:sa');
-        $('#{{ $relatedTableId }}').DataTable({
-            data: [
-                @yield($relatedTableId . 'Data')
-            ],
-            columns: [
-                @yield($relatedTableId . 'Columns')
-            ],
-            columnDefs: [
-                @yield($relatedTableId . 'ColumnDefs')
-            ],
-            "paging": true,
-            "lengthChange": false,
-            "dom": "<'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'pf>>",
-            "language": {
-                "search": "<div class='has-feedback'>_INPUT_<span class='glyphicon glyphicon-search form-control-feedback'></span></div>",
-                "searchPlaceholder": "Search..."
-            },
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": true,
-            "order": {!! $defaultOrder or '[[0, "asc"]]' !!}
-        });
+    $.extend(true, $.fn.dataTable.defaults, {
+        dom: "<'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>",
+        paging: true,
+        lengthChange: false,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: true,
+        order: {!! $defaultOrder or '[[0, "asc"]]' !!}
     });
 </script>
 @endpush
--- resources/views/crud/related-box.blade.php
+++ resources/views/crud/related-box.blade.php
@@ -3,19 +3,10 @@ $relatedTableId = isset($relatedTableId) && !empty($relatedTableId)
     ? $relatedTableId
     : 'relatedTable';
 ?>
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">{!! $boxTitle or 'Related' !!}</h3>
-        <div class="box-tools pull-right">
-            @yield('box-tools')
-        </div>
-    </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-        <table id="{{ $relatedTableId }}" class="table table-hover table-bordered"></table>
-    </div>
-    <!-- /.box-body -->
-</div>
+
+@include('wmcms::crud.datatable-box-mini', [
+    'tableId' => $relatedTableId
+])
 
 @push('js')
 <script>
@@ -30,22 +21,8 @@ $relatedTableId = isset($relatedTableId) && !empty($relatedTableId)
             ],
             columnDefs: [
                 @yield($relatedTableId . 'ColumnDefs')
-            ],
-            "paging": true,
-            "lengthChange": false,
-            "dom": "<'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'pf>>",
-            "language": {
-                "search": "<div class='has-feedback'>_INPUT_<span class='glyphicon glyphicon-search form-control-feedback'></span></div>",
-                "searchPlaceholder": "Search..."
-            },
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": true,
-            "order": {!! $defaultOrder or '[[0, "asc"]]' !!}
+            ]
         });
     });
-    $('#{{ $relatedTableId }}_filter').appendTo($('#{{ $relatedTableId }}').closest('div.box').find('div.box-header div.box-tools'));
-    console.log($('#{{ $relatedTableId }}_filter'));
 </script>
 @endpush
