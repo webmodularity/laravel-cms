30f070749170e1626c70c1cf489c6f6bb5271135
Rory Hellyer <rory@webmodularity.com>
2017-07-18 09:19:06 -0700

    dev

--- resources/views/crud/create-recent-box.blade.php
+++ resources/views/crud/create-recent-box.blade.php
@@ -1,21 +1,22 @@
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">{!! $boxTitle !!}</h3>
+@push('js')
+<script>
+    $(function () {
+        WMCMS.DT.TABLES['recent'] = $('#recent').DataTable({
+            data: [
+                @yield('recentData')
+            ],
+            columns: [
+                @yield('recentColumns')
+            ],
+            columnDefs: [
+                @yield('recentColumnDefs')
+            ],
+            order: {!! $defaultOrder or '[[0, "asc"]]' !!}
+        });
+    });
+</script>
+@endpush
 
-        <div class="box-tools pull-right">
-            @yield('box-tools')
-        </div>
-    </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-        <table id="recently-added" class="table table-hover table-bordered">
-            <thead>
-            @yield('recent-header')
-            </thead>
-            <tbody>
-            @yield('recent-rows')
-            </tbody>
-        </table>
-    </div>
-    <!-- /.box-body -->
-</div>
+@include('wmcms::crud.datatable-box-mini', [
+    'tableId' => 'recent'
+])
