7a8630b4b2024e3b59867c166f4ddb5801247ec1
Rory Hellyer <rory@webmodularity.com>
2017-08-28 16:21:38 -0700

    dev

---
+++ resources/views/crud/show-box.blade.php
@@ -0,0 +1,15 @@
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">{!! $boxTitle !!}</h3>
+        <div class="box-tools pull-right">
+            @yield('box-tools')
+            <label class="label label-primary">ID: {{ $recordId }}</label>
+        </div><!-- /.box-tools -->
+    </div>
+    <!-- /.box-header -->
+    <!-- form start -->
+    <div class="box-body">
+    @yield('form-show')
+    <!-- /.box-body -->
+    </div>
+</div>
