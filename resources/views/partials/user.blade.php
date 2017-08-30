f7909ae03cf73beea49897e0252657e207e31266
Rory Hellyer <rory@webmodularity.com>
2017-08-30 13:37:59 -0700

    dev

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -1,8 +1,8 @@
 @if(!empty($user->person->first_name))
-    <div class="form-control-static">
+    <div class="{{ isset($formStatic) && $formStatic ? 'form-control-static' : ''}}">
         <div>@include('wmcms::partials.name-full', ['person' => $user->person])</div>
         <div><a href="{{ route('users.edit', ['id' => $user->id]) }}">{{ $user->person->email }}</a></div>
     </div>
 @else
-    <p class="form-control-static"><a href="{{ route('users.edit', ['id' => $user->id]) }}">{{ $user->person->email }}</a></p>
+    <p class="{{ isset($formStatic) && $formStatic ? 'form-control-static' : ''}}"><a href="{{ route('users.edit', ['id' => $user->id]) }}">{{ $user->person->email }}</a></p>
 @endif
