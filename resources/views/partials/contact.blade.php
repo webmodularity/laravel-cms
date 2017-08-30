4d13fc01cc8d3b5fc3b8556bb9239d626b7f0efa
Rory Hellyer <rory@webmodularity.com>
2017-08-30 15:20:36 -0700

    dev

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -1,7 +1,19 @@
 @if(!empty($person['first_name']))
     <div class="{{ isset($formStatic) && $formStatic ? 'form-control-static' : ''}}">
         <div>@include('wmcms::partials.name-full', ['person' => $person])</div>
-        <div><span class="text-muted">{{ $person['email'] }}</span></div>
+        <div>
+            @if(isset($link) && $link)
+                <a href="{{ $link }}">
+            @else
+                <span class="text-muted">
+            @endif
+            {{ $person['email'] }}
+            @if(isset($link) && $link)
+                </a>
+            @else
+                </span>
+            @endif
+        </div>
     </div>
 @else
     <p class="{{ isset($formStatic) && $formStatic ? 'form-control-static' : ''}}">{{ $person->email }}</p>
