28064e14ae88eb801722e8821b1c037e1e996dbf
Rory Hellyer <rory@webmodularity.com>
2017-08-30 12:35:17 -0700

    dev

---
+++ resources/views/partials/user.blade.php
@@ -0,0 +1,8 @@
+@if(!empty($user->person->first_name))
+    <div>
+        <div>@include('wmcms::partials.name-full', ['person' => $user->person])</div>
+        <div><a href="{{ route('users.edit', ['id' => $user->id]) }}">{{ $user->person->email }}</a></div>
+    </div>
+@else
+    {{ $user->person->email }}
+@endif
